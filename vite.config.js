70b9288af508372645a72ff341407b21f8ff4bef
Kevinson Servius <serviuskevinson@hotmail.com>
2026-01-13 16:14:54 -0500

    Reparation alias @

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from "path"
 
 export default defineConfig({
   plugins: [
@@ -31,4 +32,9 @@ export default defineConfig({
       }
     })
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
